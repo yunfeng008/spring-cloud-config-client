04b9e89c0531057d6ca5eb7e5a8d0eaceb9128c8
dzp <614495906@qq.com>
2019-03-19 15:42:40 +0800

    基于springboot2.1.3的配置中心客户端

---
+++ src/main/java/com/trecco/dzp/SpringCloudConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.trecco.dzp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringCloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/trecco/dzp/web/HelloController.java
@@ -0,0 +1,19 @@
+package com.trecco.dzp.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+class HelloController {
+
+    @Value("${dzp.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String hi() {
+        return this.hello;
+    }
+}
